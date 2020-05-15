f5145596138be18fc3d1b42f88460155f89838d9
liangchengj <liangchengj@outlook.com>
2020-05-16 00:47:50 +0800

    2020/5/16 0:47

--- target/classes/static/js/amuse.js
+++ target/classes/static/js/amuse.js
@@ -2,9 +2,32 @@
  * @author Liangcheng Juves
  * Created at 2020/05/14 16:57
  */
-const doc = document
+let doc = document
 
 
-function load_font() {
+function docWrite(text) {
+    doc.write(text);
+}
+
+
+function scriptOf(src) {
+    docWrite('<script src="' + src + '"></script>')
+}
+
+function scriptAsyncOf(src) {
+    docWrite('<script async src="' + src + '"></script>')
+}
+
+
+function loadHeadCommon(title) {
+    docWrite('<meta charset="UTF-8">' +
+        '<meta name="viewport"content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">' +
+        '<meta http-equiv="X-UA-Compatible"content="ie=edge">' +
+        '<title>' + title + '</title>' +
+        '<link rel="icon"type="image/x-icon"href="/static/img/favicon.ico">' +
+        '<link rel="shortcut icon"type="image/x-icon"href="/static/img/favicon.ico">')
+}
+
+function loadJQuery() {
 
 }
